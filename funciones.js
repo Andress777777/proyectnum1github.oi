db53b7acd40e6968715ae91242d51c850819aaf0
Andress777777 <111743941+Andress777777@users.noreply.github.com>
2023-01-21 06:01:39 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,6 @@
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Experiecias: Trabajé 4 años como recepcionísta y administrativo en un hostel; 1 año como marinero mercante en barcos de pesca; 1 año de seguridad privada en varios puestos (estaciones de servicios,  supermercados, etc.); trabajo actualmente como peluquero, también pinto cuadros y los vendo de  manera ambulante y, toco la batería y canto en un grupo de música en la Iglesia."
+}
+
+
